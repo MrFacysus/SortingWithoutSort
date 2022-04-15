f2560365b8355b388e4fcdded7a4ef0c67ab54d6
MrFacysus <noobzy99@gmail.com>
2022-04-15 13:07:31 +0200

    Added Automation to Func Checking

--- SortingWithoutSort/Program.cs
+++ SortingWithoutSort/Program.cs
@@ -8,7 +8,7 @@ namespace SortingWithoutSort
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             string Inputs = Console.ReadLine();
             List<int> UnSorted = new List<int>();
@@ -18,24 +18,37 @@ namespace SortingWithoutSort
                 UnSorted.Add(int.Parse(Inputs.Split(' ')[i]));
             }
 
-            //Actual Sorting part of code
-            for (int i = 0; i < UnSorted.Count; i++)
-            {
-                for (int j = i + 1; j < UnSorted.Count; j++)
-                {
-                    if (UnSorted[i] > UnSorted[j])
-                    {
-                        int temp1 = UnSorted[i];
-                        int temp2 = UnSorted[j];
+            CheckTimeOfSorting(UnSorted, SortWithTwoLoops, "Two Loops");
+        }
 
-                        UnSorted[i] = temp2;
-                        UnSorted[j] = temp1;
+        public static void CheckTimeOfSorting(List<int> UnSorted, Func<List<int>, List<int>> SortingFunc, string Out)
+        {
+            DateTime Start = DateTime.Now;
+            SortingFunc(UnSorted);
+            DateTime End = DateTime.Now;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{(End - Start).TotalMilliseconds}ms --- {Out}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static List<int> SortWithTwoLoops(List<int> UnSorted)
+        {
+            List<int> Result = UnSorted;
+            for (int i = 0; i < Result.Count; i++)
+            {
+                for (int j = i + 1; j < Result.Count; j++)
+                {
+                    if (Result[i] > Result[j])
+                    {
+                        int temp1 = Result[i];
+                        int temp2 = Result[j];
+
+                        Result[i] = temp2;
+                        Result[j] = temp1;
                     }
                 }
             }
-            // End of sorting
-
-            Console.WriteLine(String.Join(" ", UnSorted));
+            return Result;
         }
     }
 }
